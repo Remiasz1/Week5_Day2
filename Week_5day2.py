bd98ccd08419f39fbd813a624c6c8e66d37c0b01
Remiasz1 <dremiasz@cps.edu>
2024-10-01 14:44:57 -0500

    Update Week_5day2.py

--- Week_5day2.py
+++ Week_5day2.py
@@ -44,3 +44,96 @@ friends.append("Dominik")
 friends.append("Gerdo")
 print(f"The List of friends are: {friends}")
 friends[-1] = "Dom"
+###############sets###############
+# sets are unordered and unindexed
+# they are defined using curly brakcets
+# they do not allow duplicates
+# fruits = {"apple", "banana", "mango", "cherry", "watermelon"}
+# print(fruits)
+# print(dir(fruits)) #Method can be used with sets
+# print(help(fruits)) #Documentation/methods that caqn be used with sets
+# print(len(fruits)) #Number of elements in set
+# print("Volve" in fruits) #Checks if an element is in fruits
+# print(fruits.add("orange"))
+# print (fruits)
+# print(fruits.add("kiwi"))
+# print(fruits)
+#add multiple elements to set
+# print(fruits.update(["orange", "kiwi", "pineapple"]))
+# print(fruits)
+# print(fruits.pop()) #removes the last element in set
+# print(fruits)
+# print(fruits.clear) #clears set
+# print(fruits)
+#When to use sets
+#Sets are useful when you want to store a
+#collection of items that should not be changed
+#and you don't care about the order of the items
+# social_security_number = {123456789, 2468, 13579}
+# remove last element from step
+# print(social_security_number.pop())
+# print(social_security_number)
+# add more
+# print(social_security_number.add(123789))
+# print(social_security_number)
+
+#####################uples####################
+example_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(example_tuple)
+# print(example_tuple.count(2))
+# #Counts number of times 2 is in set
+# print(dir(example_tuple))
+# #methods thac be used with set
+# print(help(example_tuple))
+# #Documentation/methods in tuple
+# print(len(example_tuple))
+# #number of elements in a tuple
+# print(2 in example_tuple)
+# #checks if element is in tuple
+# #Tuples are useful when you want to store a collection of items that should not be changed
+# print(example_tuple.index(2))
+
+# lymeric = "peter piper picked a peck of pickled pepper"
+# #convert string in tuple
+# #split first
+# lymeric_tuple = tuple(lymeric.split())
+# print(lymeric_tuple)
+# print(lymeric_tuple.count("peck"))
+# print(lymeric_tuple.count("peppers"))
+# # loop with tuples
+# for item in example_tuple:
+#     print(item)
+
+
+#dictonary
+#dictonaries are unordered, changeable and index
+#defined by curly brakets
+# has keys and values
+# keys are unique
+#values can be any data type
+capitals = {"Kenya":"Nairobi",
+            "Uganda" : "Kampala",
+             "Tanzania" : "dodoma",
+              "Rwanda": "Kligali",
+               "China": "Bejing",
+                "U.S": "Washington D.C."}
+print(capitals) 
+# #dir, help and len can be used for dictionary
+# print(dir(capitals)) # methods that can be used in dictonaries
+# print(help(capitals)) # methods/documentation that can be used with dictonaries
+# print(lens(capitals)) # Want top check the value of the key in the dictionary
+#add an item
+capitals.update({"Nigeria": "Abuja", "Spain": "Madrid","Korea": "Seoul"})
+print(capitals)
+# capital.clear()
+# loop through dictionary
+for key in capitals:
+    print(f"what are in {key}")
+
+# print values in dictionary
+for value in capitals.values():
+    print(value)
+ #print the key value pairs in dictionary
+    items_all = capitals.items()
+for key, value in items_all:
+    print(f"{key} is the capital of {value}")
